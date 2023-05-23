e07bb56e7e09772e4bd04bc1e69776ef9076d7f7
4mm449 <ammaarmuhammad509@gmail.com>
2023-05-23 13:34:36 +0800

    Half done program

--- character.py
+++ character.py
@@ -1,15 +1,26 @@
 import random
 class Character:
     def __init__(self, name, profession, unit):
-        self.name = name
-        self.profession = profession
-        self.HP = 100
-        self.ATK = 0
-        self.DEF = 0
-        self.EXP = 0
-        self.LEVEL = 1
-        self.RANK = "Bronze"
+        self.attrib = {
+            "Name" : name,
+            "Profession": profession,
+            "HP" : 100,
+            "ATK" : 0,
+            "DEF" : 0,
+            "EXP" : 0,
+            "Level" : 1,
+            "Rank" : "Bronze" 
+        }
         self.UNIT = unit
+        # self.name = name
+        # self.profession = profession
+        # self.HP = 100
+        # self.ATK = 0
+        # self.DEF = 0
+        # self.EXP = 0
+        # self.LEVEL = 1
+        # self.RANK = "Bronze"
+        # self.UNIT = unit
         
         # player chooses warrior
         if profession == 'W':
@@ -21,29 +32,30 @@ class Character:
             print("[-] ERROR. INVALID INPUT. Please enter W for Warrior and T for Tanker [-]")
         # display the stats
         print("---------------------------"\
-            f"\nPlayer Unit {self.UNIT}: "\
-            "\n---------------------------"\
-            "\n(1) Name: ", self.name,\
-            "\n(2) Profession: ",self.profession,\
-            "\n(3) Health Point (HP): ",self.HP,\
-            "\n(4) Attack Point (ATK): ",self.ATK,\
-            "\n(5) Defence Point (DEF): ",self.DEF,\
-            "\n(6) Experience (EXP): ",self.EXP,\
-            "\n(7) Rank: ", self.RANK,
+            # f"\nPlayer Unit {self.UNIT}: "\
+            # "\n---------------------------"\
+            "\n(1) Name: ", self.attrib["Name"],\
+            "\n(2) Profession: ",self.attrib["Profession"],\
+            "\n(3) Health Point (HP): ",self.attrib["HP"],\
+            "\n(4) Attack Point (ATK): ",self.attrib["ATK"],\
+            "\n(5) Defence Point (DEF): ",self.attrib["DEF"],\
+            "\n(6) Experience (EXP): ",self.attrib["EXP"],\
+            "\n(7) Rank: ", self.attrib["Rank"],
             "\n---------------------------\n")
     def __str__(self) -> str:
-        return f"Name: {self.name}, Profession: {self.profession}, HP: {self.HP}, ATK: {self.ATK}, DEF: {self.DEF}, EXP: {self.EXP}, Rank: {self.RANK}"
+        return "Name: {Name}, Profession: {Profession}, HP: {HP}, ATK: {ATK}, DEF: {DEF}, EXP: {EXP}, Rank: {Rank}" .format(**self.attrib)
+        # return f"{{'Name': '{self.name}','Profession': '{self.profession}', 'HP': {self.HP}, 'ATK': {self.ATK}, DEF: {self.DEF}, EXP: {self.EXP}, Rank: '{self.RANK}'}}"
     # setting up warrior
     def setup_warrior(self):
-        self.ATK = random.randint(5, 20)
-        self.DEF = random.randint(1,20)
-        self.profession = 'Warrior'
+        self.attrib["ATK"] = random.randint(5, 20)
+        self.attrib["DEF"] = random.randint(1,20)
+        self.attrib["Profession"] = 'Warrior'
         
     # setting up tanker
     def setup_tanker(self):
-        self.ATK = random.randint(1, 10)
-        self.DEF = random.randint(5, 15)
-        self.profession = 'Tanker'
+        self.attrib["ATK"] = random.randint(1, 10)
+        self.attrib["DEF"] = random.randint(5, 15)
+        self.attrib["Profession"] = 'Tanker'
 
         
     # defining the attack logic
@@ -52,14 +64,7 @@ class Character:
         target.HP -= damage
         print(f"{self.name} attacked {target.name} and dealt {damage} damage")
 
-    # main battle
-    def game(self):
-        game_over = True
-        try_again = True
-        while game_over:
-            while try_again:
-                if self.HP == 100:
-                    print("100")   
+    
 # player unit        
 class Player:
     def __init__(self):
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import character
+import random
 player_units = []
 ai_units = []
-# game_over = False
+game_over = False
+rounds = 1
 UNIT = 3
 RANK = ["Bronze", "Silver", "Gold", "Diamond"]
 # main function
 def main():
     game_intro()
     unit = 0
-    # prompt user for player unit details
+    
+    # prompt user for player unit details, setup player units
     for i in range(UNIT):
         name = input(f"(1) Enter the name for Unit {i + 1}: ")
         profession = input(f"(2) Choose either (W)arrior or (T)anker for Unit {i + 1}: ")
@@ -19,22 +22,103 @@ def main():
                 if profession[0].upper() == 'W' or profession[0].upper() == 'T':
                     profession = profession[0].upper()
                     temp_player_units = str(character.Character(name, profession, unit))
+                    print(temp_player_units)
                     # player_units = character.Player.add_unit(temp_player_units)
-                    player_units.append(str(temp_player_units))
+                    player_units.append(temp_player_units)
                     print(player_units)
                     profession_check = False
                 else:
                  profession = input(f"[-] ERROR, BAD INPUT. Enter W for Warrior or T for Tanker for Unit {i + 1}: ")
             except:
                 profession = input("[-] ERROR. No value specified. Please specify a value [W/T]: ")
-    game =  character.Character(name, profession, unit)
-    game.game()
+   
+    player_unit1 = player_units[0]
+    player_unit2 = player_units[1]
+    player_unit3 = player_units[2]
     
+    # unit = 0
+    # setup AI units
+    choice = ["W", "T"]
+    for i in range(UNIT):
+        aname = f"AI{random.randint(1, 99)}"
+        aprofession = random.choice(choice)
+        temp_ai_units = str(character.Character(aname, aprofession, unit))
+        ai_units.append(temp_ai_units)
+        
+    ai_unit1 = ai_units[0]
+    ai_unit2 = ai_units[1]
+    ai_unit3 = ai_units[2]
+        
+    game =  character.Character(name, profession, unit)
+    try_again = True
+    while game_over == False:
+        while try_again:
+            if rounds % 2 == 1:
+                print(f"It's {player_name}'s turn to attack")
+                display_stats = True
+                while display_stats:
+                    attacker = input("Enter unit that you wish to send for an attack [1-3] or type U to check unit stats: ")
+                    try:
+                        if attacker == "U":
+                            # display_gamestats()
+                            print(player_units)
+                            print(ai_units)
+                            display_stats = False
+                            attacker = input("Enter unit that you wish to send for an attack [1-3]: ")
+                            victim = input("Enter AI unit that you wish to attack [1-3]: ")
+                        else:
+                            victim = input("Enter AI unit that you wish to attack [1-3]: ")
+                    except:
+                        attacker = input("[-] ERROR. No value specified. Please specify a value [1-3 or U]: ")
+                    
+                    #AI chooses attack
+
+                    victim = random.choice(player_units)
+                    attacker = random.choice(ai_units)   
+                    
   
 
 
                  
                  
+# print(game stats)
+def display_gamestats():
+    # player_units = [player_unit1, player_unit2, player_unit3]
+    # ai_units = [ai_unit1, ai_unit2, ai_unit3]
+    # print("---------------------------\n")
+
+    # print("---------------------------")
+    # print("Player Unit {}:            |\n---------------------------\n(1) Name: {Name}\n(2) Profession: {Profession}\n(3) Health Point (HP): {HP}\n(4) Attack Point (ATK): {ATK}\n(5) Defence Point (DEF): {DEF}\n(6) Experience (EXP): {EXP}\n(7) Rank: {Rank}" .format(i + 1, **player_unit))
+    # print("---------------------------\n")
+    # print(f"AI TEAM:\n\nAI Unit 1: {ai_unit1}\nAI Unit 2: {ai_unit2}\nAI Unit 3: {ai_unit3}\n")
+    #display player stats
+    print("---------------------------")
+    print("Player Unit 1:             |\n---------------------------\n(1) Name: {Name}\n(2) Profession: {Profession}\n(3) Health Point (HP): {HP}\n(4) Attack Point (ATK): {ATK}\n(5) Defence Point (DEF): {DEF}\n(6) Experience (EXP): {EXP}\n(7) Rank: {Rank}" .format(i + 1, **player_unit1))
+    print("---------------------------\n")
+
+    print("---------------------------")
+    print("Player Unit 2:             |\n---------------------------\n(1) Name: {Name}\n(2) Profession: {Profession}\n(3) Health Point (HP): {HP}\n(4) Attack Point (ATK): {ATK}\n(5) Defence Point (DEF): {DEF}\n(6) Experience (EXP): {EXP}\n(7) Rank: {Rank}" .format(i + 1, **player_unit2))
+    print("---------------------------\n")
+
+    print("---------------------------")
+    print("Player Unit 3:             |\n---------------------------\n(1) Name: {Name}\n(2) Profession: {Profession}\n(3) Health Point (HP): {HP}\n(4) Attack Point (ATK): {ATK}\n(5) Defence Point (DEF): {DEF}\n(6) Experience (EXP): {EXP}\n(7) Rank: {Rank}" .format(i + 1, **player_unit3))
+    print("---------------------------\n")
+
+
+    print("***************************\n")
+
+    #display ai stats
+    print("---------------------------")
+    print("AI Unit 1:                 |\n---------------------------\n(1) Name: {Name}\n(2) Profession: {Profession}\n(3) Health Point (HP): {HP}\n(4) Attack Point (ATK): {ATK}\n(5) Defence Point (DEF): {DEF}\n(6) Experience (EXP): {EXP}\n(7) Rank: {Rank}" .format(i + 1, **ai_unit1))
+    print("---------------------------\n")
+
+    print("---------------------------")
+    print("AI Unit 2:                 |\n---------------------------\n(1) Name: {Name}\n(2) Profession: {Profession}\n(3) Health Point (HP): {HP}\n(4) Attack Point (ATK): {ATK}\n(5) Defence Point (DEF): {DEF}\n(6) Experience (EXP): {EXP}\n(7) Rank: {Rank}" .format(i + 1, **ai_unit2))
+    print("---------------------------\n")
+
+    print("---------------------------")
+    print("AI Unit 3:                 |\n---------------------------\n(1) Name: {Name}\n(2) Profession: {Profession}\n(3) Health Point (HP): {HP}\n(4) Attack Point (ATK): {ATK}\n(5) Defence Point (DEF): {DEF}\n(6) Experience (EXP): {EXP}\n(7) Rank: {Rank}" .format(i + 1, **ai_unit3))
+    print("---------------------------\n")
 
 def game_intro():
     banner_text = '''                                            
