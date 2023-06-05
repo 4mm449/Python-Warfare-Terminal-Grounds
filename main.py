8cfc3fbf2825f5239274a1556abe0542bf216357
4mm449 <ammaarmuhammad509@gmail.com>
2023-06-05 12:34:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,10 @@ except ImportError:
   print("Trying to Install required module: tabulate\n")
 if platform == "darwin":
     os.system('pip3 install tabulate')
+    quit('Tabulate successfully installed, please run the program again')
 elif platform == "win32":
     os.system('pip install tabulate')
+
   # os.system('python -m pip install tabulate')
 # above lines try to install tabulate module if not present
 # if all went well, import required module again
